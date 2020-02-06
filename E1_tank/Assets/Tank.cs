e2fff4c2b971ffeadf5030d15d7c063b66b4d930
Silas Praat <silaspraat@gmail.com>
2020-02-06 16:17:52 +0100

    bla

--- E1_tank/Assets/Tank.cs
+++ E1_tank/Assets/Tank.cs
@@ -10,29 +10,27 @@ public class Tank : MonoBehaviour
     Transform turret;
     Transform barrel;
 
-    void Start()
+    void Awake()
     {
-        Debug.Log(gameObject.name);
         turret = transform.Find("Turret");
         barrel = transform.Find("Turret/BarrelTip");
     }
 
     void Update()
     {
+		
         if (Input.GetButtonDown("Fire")) fire();
-    }
+        turret.transform.Rotate(0.0f, Time.deltaTime * 30 * Input.GetAxisRaw("RotateTurret"), 0.0f, Space.Self);
 
-    private void FixedUpdate()
-    {
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         movement = Vector3.ClampMagnitude(movement, 1) * speed * Time.deltaTime;
         transform.Translate(movement);
-        turret.localEulerAngles += new Vector3(0, 1, 0) * Time.deltaTime * 30 * Input.GetAxisRaw("RotateTurret");
+
         RaycastHit hit;
-        if (Physics.Raycast(transform.position + transform.forward*0.5f, transform.up * -1, out hit, 25.0f)) // cast at the front so we don't risk hitting previous plane after rotation
+        if (Physics.Raycast(transform.position + transform.forward * 0.5f, transform.up * -1, out hit, 25.0f)) // cast at the front so we don't risk hitting previous plane after rotation
         {
             transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
-            transform.Translate(transform.up * -1 * (hit.distance - hoverHeight)); 
+            transform.Translate(transform.up * -1 * (hit.distance - hoverHeight));
         }
     }
 
